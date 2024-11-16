c31e20fa4a2bb02cc884a50f20aa03e5a16459c7
jafh1906 <juanfrancoclasesvirtuales@gmail.com>
2024-11-16 09:39:06 -0500

    se agrego la page

---
+++ main.js
@@ -0,0 +1,394 @@
+let aptos = [
+    {
+        img:"imagenes/APTO-1.png",
+        costo_detalle1:"$3,500+ Studio ",
+        costo_detalle2:"$3,650+ 2bds | ",
+        costo_detalle3:"$5,000+ 3bds",
+        n_edificio:"The Tides At Arverne By The Sea |",
+        ubicacion:"190 Beach 69t...",
+        estados:"rent",
+        habitaciones:"3"
+    },
+
+    {
+        img:"imagenes/APTO-2.png",
+        costo_detalle1:"$3,075+ 1 bd ",
+        costo_detalle2:"$4,000+ 2bds |",
+        costo_detalle3:"$5,795+ 3 bds ", 
+        n_edificio:"The Maven |",
+        ubicacion:"2413 3rd Ave, Bronx, NY",
+        estados:"rent",
+        habitaciones:"3"
+    },
+
+    {
+        img:"imagenes/APTO-3.png",
+        costo_detalle1:"$3,529+ Studio ",
+        costo_detalle2:"$4,171+ 1 bd | ",
+        costo_detalle3:"6.325 2 bds |",
+        costo_detalle4:"$9,121+ 3 bds",
+        n_edificio:"West Wharf |",
+        ubicacion:"60 Wharf Dr, Brooklyn, NY",
+        estados:"rent",
+        habitaciones:"3"
+    },
+
+    {
+        img:"imagenes/APTO-4.png",
+        costo_detalle1:"$4,395+ Studio ",
+        costo_detalle2:"$5,679+ 1 bd | ",
+        costo_detalle3:"7.852 2 bds",
+        n_edificio:"One Domino Square |",
+        ubicacion:"5 S 5th St, Brooklyn, NY",
+        estados:"rent",
+        habitaciones:"2"
+    },
+
+    {
+        img:"imagenes/APTO-5.png",
+        costo_detalle1:"$3,100+ 2 bds ",
+        costo_detalle2:"$3,900 3 bds | ",
+        n_edificio:"The Agora |",
+        ubicacion:"2667 Fulton St, Brooklyn, NY",
+        estados:"sale",
+        habitaciones:"2"
+    },
+
+    {
+        img:"imagenes/APTO-6.png",
+        costo_detalle1:"$4,263+ Studio ",
+        costo_detalle2:"$4,858 1 bd | ",
+        n_edificio:"The Ellery |",
+        ubicacion:"312 W 43rd St, New York, NY",
+        estados:"sale",
+        habitaciones:"1"
+    },
+
+    {
+        img:"imagenes/APTO-7.png",
+        costo_detalle1:"$3,530+ Studio ",
+        costo_detalle2:"$4,929+ 1 bd | ",
+        costo_detalle3:"7,340 2 bds",
+        n_edificio:"Magnolia Dumbo",
+        ubicacion:"170 Front St, Brooklyn, NY",
+        estados:"sale",
+        habitaciones:"1"
+    },
+
+    {
+        img:"imagenes/APTO-8.png",
+        costo_detalle1:"$3,200+ Studio ",
+        costo_detalle2:"$3,525+ 1 bd | ",
+        costo_detalle3:"4,300 2 bds",
+        n_edificio:"Luxe By Russos",
+        ubicacion:"241 Northern Blvd Little Neck...",
+        estados:"sold",
+        habitaciones:"2"
+    },
+
+    {
+        img:"imagenes/APTO-9.png",
+        costo_detalle1:"$3,545+ Studio ",
+        costo_detalle2:"$3,687+ 1 bd | ",
+        costo_detalle3:"5,152 2 bds",
+        n_edificio:"Ava DoBro |",
+        ubicacion:"100 Willoughby St, Brooklyn, NY",
+        estados:"sold",
+        habitaciones:"2"
+    },
+    
+    {
+        img:"imagenes/APTO-10.png",
+        costo_detalle1:"$4,205+ Studio ",
+        costo_detalle2:"$5,373+ 1 bd | ",
+        costo_detalle3:"8,347 2 bds",
+        n_edificio:"The Copitol |",
+        ubicacion:"776 6th Ave, New York, NY",
+        estados:"sold",
+        habitaciones:"1"
+    },
+]
+
+let pop_up = [
+    {
+        img_1:"imagenes/emergente_1-apto1.png",
+        img_2:"imagenes/emergente_2-apto1.png",
+        img_3:"imagenes/emergente_3-apto1.png",
+        img_4:"imagenes/emergente_4-apto1.png",
+        n_edificio:"The Tides At Arverne By The Sea",
+        ubicacion:"190 Beach 69th St, Arverne, NY 11692",
+        sitio_web:"Visit The Tides At Arverne By The Sea website",
+    },
+
+    {
+        img_1:"imagenes/emergente_1-apto2.png",
+        img_2:"imagenes/emergente_2-apto2.png",
+        img_3:"imagenes/emergente_3-apto2.png",
+        img_4:"imagenes/emergente_4-apto2.png",
+        n_edificio:"The Maven",
+        ubicacion:"2413 3rd Ave, Bronx, NY 10451",
+        sitio_web:"Visit The Maven website",
+    },
+
+    {
+        img_1:"imagenes/emergente_1-apto3.png",
+        img_2:"imagenes/emergente_2-apto3.png",
+        img_3:"imagenes/emergente_3-apto3.png",
+        img_4:"imagenes/emergente_4-apto3.png",
+        n_edificio:"West Wharf",
+        ubicacion:"60 Wharf Dr, Brooklyn, NY 11222",
+        sitio_web:"Visit West Wharf website",
+    },
+
+    {
+        img_1:"imagenes/emergente_1-apto4.png",
+        img_2:"imagenes/emergente_2-apto4.png",
+        img_3:"imagenes/emergente_3-apto4.png",
+        img_4:"imagenes/emergente_4-apto4.png",
+        n_edificio:"One Domino Square",
+        ubicacion:"5 S 5th St, Brooklyn, NY 11249",
+        sitio_web:"Visit One Domino Square website",
+    },
+
+    {
+        img_1:"imagenes/emergente_1-apto5.png",
+        img_2:"imagenes/emergente_2-apto5.png",
+        img_3:"imagenes/emergente_3-apto5.png",
+        img_4:"imagenes/emergente_4-apto5.png",
+        n_edificio:"The Agora",
+        ubicacion:"2667 Fulton St, Brooklyn, NY 11207",
+        sitio_web:"Visit The Agora website",
+    },
+
+    {
+        img_1:"imagenes/emergente_1-apto6.png",
+        img_2:"imagenes/emergente_2-apto6.png",
+        img_3:"imagenes/emergente_3-apto6.png",
+        img_4:"imagenes/emergente_4-apto6.png",
+        n_edificio:"The Ellery",
+        ubicacion:"312 W 43rd St, New York, NY 10036",
+        sitio_web:"Visit The Ellery website",
+    },
+
+    {
+        img_1:"imagenes/emergente_1-apto7.png",
+        img_2:"imagenes/emergente_2-apto7.png",
+        img_3:"imagenes/emergente_3-apto7.png",
+        img_4:"imagenes/emergente_4-apto7.png",
+        n_edificio:"Magnolia Dumbo",
+        ubicacion:"160 Front St, Brooklyn, NY 11201",
+        sitio_web:"Visit Magnolia Dumbo website",
+    },
+
+    {
+        img_1:"imagenes/emergente_1-apto8.png",
+        img_2:"imagenes/emergente_2-apto8.png",
+        img_3:"imagenes/emergente_3-apto8.png",
+        img_4:"imagenes/emergente_4-apto8.png",
+        n_edificio:"Luxe By Russos",
+        ubicacion:"241 Northern Blvd, Douglaston, NY 11362",
+        sitio_web:"Visit Luxe By Russos website",
+    },
+
+    {
+        img_1:"imagenes/emergente_1-apto9.png",
+        img_2:"imagenes/emergente_2-apto9.png",
+        img_3:"imagenes/emergente_3-apto9.png",
+        img_4:"imagenes/emergente_4-apto9.png",
+        n_edificio:"AVA DoBro",
+        ubicacion:"100 Willoughby St, Brooklyn, NY 11201",
+        sitio_web:"Visit AVA DoBro website",
+    },
+
+    {
+        img_1:"imagenes/emergente_1-apto10.png",
+        img_2:"imagenes/emergente_2-apto10.png",
+        img_3:"imagenes/emergente_3-apto10.png",
+        img_4:"imagenes/emergente_4-apto10.png",
+        n_edificio:"The Capitol",
+        ubicacion:"776 6th Ave, New York, NY 10001",
+        sitio_web:"Visit The Capitol website",
+    }
+
+]
+
+function mostrar_carta(propiedades){
+    listaActual = propiedades
+    listaActual = pop_up
+    $("#subcontainer_aptos").empty();
+
+    for(let i = 0; i < propiedades.length; i++){
+
+        let cardClass = "rounded-lg w-[340px]";
+
+        $("#subcontainer_aptos").append(
+            '<div class="apto '+cardClass+' "data-index="'+i+'">'+
+                '<img class="rounded-lg" src="'+propiedades[i].img+'"/>'+
+                '<div class="pl-3.5 flex flex-col">'+
+                    '<h1 class="text-xl font-bold mt-2">'+propiedades[i].costo_detalle1+'</h1>'+
+                    '<div class="flex mt-2 opacity-85 font-medium">'+
+                        '<p class="text-sm">'+ propiedades[i].costo_detalle2 +'</p>'+
+                        '<p class="text-sm" >'+propiedades[i].costo_detalle3+'</p>'
+                    +'</div>'+
+                    '<div class="flex mt-2 mb-2 opacity-85 font-medium">'+
+                        '<p  class="text-sm">'+propiedades[i].n_edificio+'</p>'+
+                        '<p class="text-sm">'+propiedades[i].ubicacion+'</p>'+
+                    '</div>'
+                +'</div>'+
+            '</div>'
+        )
+    }
+}
+
+
+$(document).ready(function(){
+
+    mostrar_carta(aptos)
+// los eventos siempre van despues de las funciones
+
+    $("#buscar").click(function(){
+        let busqueda = $("#busqueda").val().toLowerCase().trim();
+        let arrayBusqueda = [];
+        if(busqueda == ""){
+            mostrar_carta(aptos)
+        }else{
+            for(let i = 0; i < aptos.length; i++){
+                if(aptos[i].n_edificio.toLowerCase().trim() == busqueda){
+                    arrayBusqueda.push(aptos[i])
+                }
+            } 
+            mostrar_carta(arrayBusqueda)
+        } 
+       
+    })
+
+    $("#estados").change(function(){
+
+        let estadoSelecionada = $("#estados").val();
+        let arrayEstados = [];
+
+        if(estadoSelecionada == "all"){
+            mostrar_carta(aptos)
+        }else{
+            for(let i = 0; i < aptos.length; i++){
+            
+                if(aptos[i].estados == estadoSelecionada){
+                    arrayEstados.push(aptos[i])
+                }
+            mostrar_carta(arrayEstados)
+            }
+        }
+    })
+
+    $("#habitaciones").change(function(){
+
+        let habitacioneSelecionada = $("#habitaciones").val();
+        let arrayHabitaciones = [];
+
+        if(habitacioneSelecionada == "all"){
+            mostrar_carta(aptos)
+        }else{
+            for(let i = 0; i < aptos.length; i++){
+            
+                if(aptos[i].habitaciones == habitacioneSelecionada){
+                    arrayHabitaciones.push(aptos[i])
+                }
+            mostrar_carta(arrayHabitaciones)
+            }
+        }
+    })
+
+// esta es otra manera de hacer el filtrado con change
+
+    // $("#estado").change(function(){
+    //     let opcionSelecionada = $(this).children(":selected").val();
+    //     let arrayEstados = [];
+
+    
+    // if(opcionSelecionada == "all"){
+    //     mostrar_carta(aptos)
+    // }else{
+    //     for(let i = 0; i < aptos.length; i++){
+            
+    //         if(aptos[i].estados == opcionSelecionada){
+    //             arrayEstados.push(aptos[i])
+    //         }
+    //     mostrar_carta(arrayEstados)
+    //     }
+    // }
+
+    // })
+
+    $(document).on('click', '.apto', function() {
+        let index = $(this).data("index");
+        let propiedades = listaActual[index];
+        $(".pop-up_window").removeClass("hidden")
+        $("#imagen-1").attr("src",propiedades.img_1)
+        $("#imagen-2").attr("src", propiedades.img_2)
+        $("#imagen-3").attr("src", propiedades.img_3)
+        $("#imagen-4").attr("src", propiedades.img_4)
+        $("#edificio").html(propiedades.n_edificio)
+        $("#ubicacion").html(propiedades.ubicacion)
+        $("#web").html(propiedades.sitio_web)
+        // let popUpContent =
+        // '<div class="salir absolute right-3 top-4 cursor-pointer">'+
+        //         '<img class="w-[50px]" src="imagenes/salir.png" alt="">'
+        //     +'</div>'+
+        //     '<div class="bg-white flex">'+
+        //         '<div class="imagenes-aptos flex flex-col gap-1 w-[500px] h-[650px]">'+
+        //         '<img class="img-1" src="'+propiedades.img_1+'" alt="">'+
+        //             '<div class="flex gap-1">'+
+        //                 '<img class="img-2 w-60" src="'+propiedades.img_2+'" alt="">'+
+        //                 '<img class="img-3 w-60" src="'+propiedades.img_3+'" alt="">'
+        //             +'</div>'+
+        //         '<img class="img-4" src="'+propiedades.img_4+'" alt="">'
+        //     +'</div>'+
+        //     '<div class="propiedades-apto py-3 px-6 w-[500px] bg-opacity-50">'+
+        //         '<div class="flex justify-between items-center pb-4 border-b border-[#d1d1d5]">'+
+        //             '<div class="zillow cursor-pointer">'+
+        //                 '<img class="w-[118px]" src="imagenes/logo.png" alt="">'
+        //             +'</div>'+
+        //             '<div class="flex gap-x-6">'+
+        //                 '<div class="flex items-center">'+
+        //                     '<img src="imagenes/like.png" alt="">'+
+        //                     '<p class="text-[#006aff]">Save</p>'
+        //                 +'</div>'+
+        //                 '<div class="flex items-center">'+
+        //                     '<img src="imagenes/share.png" alt="">'+
+        //                     '<p class="text-[#006aff]">Share</p>'
+        //                 +'</div>'+
+        //                 '<div class="flex items-center">'+
+        //                     '<img src="imagenes/more.png" alt="">'+
+        //                     '<p class="text-[#006aff]">More</p>'
+        //                 +'</div>'
+        //             +'</div>'
+        //         +'</div>'+
+        //         '<div class="flex flex-col gap-y-0.5 my-2">'+
+        //             '<h1 class="edificio text-2xl font-bold">'+propiedades.n_edificio+'</h1>'+
+        //             '<h2 class="ubicacion text-[#2a2a33]">'+propiedades.ubicacion+'</h2>'+
+        //             '<a class="web text-[#006aff] font-bold" href="#">'+propiedades .sitio_web+'</a>'+
+        //             '<div class="flex items-center gap-x-1">'+
+        //                 '<img src="imagenes/star.png" alt="">'+
+        //                 '<img src="imagenes/star.png" alt="">'+
+        //                 '<img src="imagenes/star.png" alt="">'+
+        //                 '<img src="imagenes/star.png" alt="">'+
+        //                 '<img src="imagenes/star.png" alt="">'+
+        //                 '<p class="text-[#055e16] font-semibold">(1)</p>'
+        //             +'</div>'
+        //         +'</div>'+
+        //         '<div class="flex gap-x-2 border-b border-[#d1d1d5] pb-3.5">'+
+        //             '<button class="border border-[#006aff] text-[#006aff] text-center py-2.5 rounded-md font-bold w-[230px]">Request to apply</button>'+
+        //             '<button class="bg-[#006aff] text-white text-center py-2.5 rounded-md font-bold w-[230px]">Request a tour</button>'
+        //         +'</div>'
+        //     +'</div>'
+        // +'</div>'
+
+
+        // $(".pop-up_window").html(popUpContent).removeClass("hidden");
+    });
+    
+    $(document).on('click', '.salir', function() {
+        $(".pop-up_window").addClass("hidden");
+    });
+
+})
